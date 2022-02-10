81e137bb6ec73e66f9cb6684fedb2c81d43399b6
Rohit Kumar Singh <93372627+rohitsingh-23@users.noreply.github.com>
2022-02-10 21:16:49 +0530

    Food-recipe-app

---
+++ nav.js
@@ -0,0 +1,11 @@
+function nav(){
+    return `<div class="nav">
+    <div class="nleft" id="home">Home</div>
+    <div class="nright">
+        <div id = "latest">Latest recipe</div>
+        <div id = "day">Recipe of the day</div>
+    </div>
+</div>`
+}
+
+export default nav
